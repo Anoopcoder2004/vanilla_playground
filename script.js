e2db96dfc5911b2beaea9575cc9ace7f82dd8975
Anoop <Anooppopziyan@gmail.com>
2026-03-02 16:30:18 +0530

    added strong password validation and email validation

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
 const form = document.getElementById("registrationForm");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const error = document.getElementById("error");
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const strongPasswordRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+
+
+function validateEmail() {
+    const email = emailInput.value.trim();
+
+    if (email === "") {
+        showError("Email is required");
+        return false;
+    }
+
+    if (!emailRegex.test(email)) {
+        showError("Invalid email address");
+        return false;
+    }
+
+    hideError();
+    return true;
+}
+
+function validatePassword(){
+    const password = passwordInput.value.trim();
+    if(password === ""){
+        showError("Password is required");
+        return false;
+    
+    }
+    if(!strongPasswordRegex.test(password)){
+        showError("Password must be at least 8 characters long and include uppercase, lowercase, number, and special character");
+        return false;
+    }
+    hideError();
+    return true;
+}
+
+function showError(message) {
+    error.textContent = message;
+    error.classList.remove("hidden");
+}
+
+function hideError() {
+    error.textContent = "";
+    error.classList.add("hidden");
+}
+
+emailInput.addEventListener("blur", validateEmail);
+passwordInput.addEventListener("blur", validatePassword);
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
     const phone = document.getElementById("phone").value.trim();
-    const email = document.getElementById("email").value.trim();
-    if (username === "" || password === '' || phone === '' || email === "") {
+
+    if (username === "" || password === "" || phone === "") {
         alert("Please fill in all the fields");
         return;
     }
-    alert("Form submitted successfully!")
+
+    if (!validateEmail() || !validatePassword()) return;
+
+    alert("Form submitted successfully!");
 });
