d3a89357408f190b895e839a37a2dd6f99642f71
Anoop <Anooppopziyan@gmail.com>
2026-03-02 11:07:49 +0530

    form with mandatory input fields

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var count = 0;
+const form = document.getElementById("registrationForm");
+form.addEventListener("submit", function (event) {
 
-function increment() {
-    count++;
-    document.getElementById("counter").innerHTML = count;
-}
-
-function decrement() {
-    if (count > 0) {
-        count--;
-        document.getElementById("counter").innerHTML = count;
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const email = document.getElementById("email").value.trim();
+    if (username === "" || password === '' || phone === '' || email === "") {
+        alert("Please fill in all the fields");
+        return;
     }
-}
+    alert("Form submitted successfully!")
+});
