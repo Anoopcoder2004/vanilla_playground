153d31466afd1d4e555044132feff1525cae4c01
Anoop <Anooppopziyan@gmail.com>
2026-03-02 11:09:01 +0530

    removed form reset on submit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("registrationForm");
 form.addEventListener("submit", function (event) {
-
+    event.preventDefault();
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
     const phone = document.getElementById("phone").value.trim();
