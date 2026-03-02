b77a298ae6ed4c3b1dca82436a037ef72f66ae01
Anoop <Anooppopziyan@gmail.com>
2026-03-02 10:23:09 +0530

    counter with negative numbers

---
+++ script.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+function increment(){
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+function decrement(){
+    count--;
+    document.getElementById("counter").innerHTML = count;
+}
