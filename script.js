99c64c16bb9bc1681bd35ada469144f22500613e
Anoop <Anooppopziyan@gmail.com>
2026-08-11 22:16:22 +0530

    Advanced JavaScript Debugging Challenge

--- script.js
+++ script.js
@@ -1,30 +1,98 @@
-const input = document.getElementById("search");
-const result = document.getElementById("result");
+const employees = [
+    {
+        id: 101,
+        name: "Anoop",
+        department: "Frontend",
+        salary: 28000,
+        active: true
+    },
+    {
+        id: 102,
+        name: "Rahul",
+        department: "Backend",
+        salary: 45000,
+        active: false
+    },
+    {
+        id: 103,
+        name: "Arjun",
+        department: "Frontend",
+        salary: 32000,
+        active: true
+    },
+    {
+        id: 104,
+        name: "Andrew",
+        department: "Backend",
+        salary: 25000,
+        active: true
+    },
+    {
+        id: 105,
+        name: "John",
+        department: "QA",
+        salary: 29000,
+        active: false
+    },
+    {
+        id: 106,
+        name: "David",
+        department: "QA",
+        salary: 27000,
+        active: true
+    }
+];
 
-input.addEventListener("input", handleSearch);
 
-function fakeApi(value) {
+function generateReport(data) {
 
-    return new Promise(resolve => {
+    const activeEmployees = data.filter(employee => {
+        return employee.active;
+    });
 
-        const delay = Math.random() * 3000;
 
-        setTimeout(() => {
+    const processedEmployees = activeEmployees.map(employee => {
 
-            resolve(value);
+        const result = employee;
 
-        }, delay);
+        if (employee.salary < 30000) {
+
+            result.salary = employee.salary * 1.10;
+
+        }
+
+        return {
+            id: result.id,
+            name: result.name,
+            department: result.department,
+            finalSalary: result.salary
+        };
 
     });
 
+
+    processedEmployees.sort((a, b) => {
+
+        return a.finalSalary < b.finalSalary;
+
+    });
+
+
+    const frontendEmployees = processedEmployees.filter(employee => {
+
+        return employee.department = "Frontend";
+
+    });
+
+
+    return frontendEmployees;
 }
 
-async function handleSearch() {
 
-    const value = input.value;
+const report = generateReport(employees);
 
-    const response = await fakeApi(value);
+console.log("FINAL REPORT");
+console.table(report);
 
-    result.textContent = response;
-
-}
+console.log("ORIGINAL DATA");
+console.table(employees);
