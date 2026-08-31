c96f85ab32e6eb1bdf9c9c1ddd432d2aba8ba8fb
Anoop <Anooppopziyan@gmail.com>
2026-08-31 23:48:07 +0530

    brower performance section practice

--- script.js
+++ script.js
@@ -1,91 +1,22 @@
-const employees = [
-    {
-        id: 1,
-        name: "Anoop",
-        department: "Frontend",
-        salary: 28000,
-        active: true
-    },
-    {
-        id: 2,
-        name: "Rahul",
-        department: "Backend",
-        salary: 45000,
-        active: true
-    },
-    {
-        id: 3,
-        name: "Arjun",
-        department: "Frontend",
-        salary: 25000,
-        active: false
-    },
-    {
-        id: 4,
-        name: "Andrew",
-        department: "QA",
-        salary: 27000,
-        active: true
+const freezeBtn = document.getElementById("freezeBtn");
+const testBtn = document.getElementById("testBtn");
+const output = document.getElementById("output");
+
+freezeBtn.addEventListener("click", () => {
+
+    console.log("Starting expensive operation...");
+
+    let result = 0;
+
+    for (let i = 0; i < 500_000_000; i++) {
+        result += Math.sqrt(i);
     }
-];
 
+    output.textContent = result;
 
-function generateReport(employees) {
+    console.log("Finished");
+});
 
-    const activeEmployees = employees.filter(employee => {
-
-        return employee.active === true;
-
-    });
-
-
-    const processedEmployees = activeEmployees.map(employee => {
-
-        if (employee.salary < 30000) {
-
-            employee.salary = employee.salary * 1.10;
-
-        }
-
-        return {
-            name: employee.name,
-            department: employee.department,
-            salary: employee.salary
-        };
-
-    });
-
-
-    processedEmployees.sort((a, b) => {
-
-        return a.salary - b.salary;
-
-    });
-
-
-    const totalSalary = processedEmployees.reduce(
-        (total, employee) => {
-
-            return total + employee.salary;
-
-        },
-        0
-    );
-
-
-    return {
-        employees: processedEmployees,
-        totalSalary: totalSalary
-    };
-}
-
-
-const report = generateReport(employees);
-
-console.log("REPORT");
-console.table(report.employees);
-
-console.log("TOTAL SALARY:", report.totalSalary);
-
-console.log("ORIGINAL EMPLOYEES");
-console.table(employees);
+testBtn.addEventListener("click", () => {
+    console.log("Test button clicked");
+});
